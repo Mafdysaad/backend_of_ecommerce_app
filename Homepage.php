4aec033b0313d5d4391234d28248d7a88a688b22
mafdysaad <mafdysaad366@gmail.com>
2024-09-07 08:40:37 +0300

    second

--- Homepage.php
+++ Homepage.php
@@ -4,6 +4,6 @@ $AllData = array();
 $data = getAllData('categories',null,null,false);
 $AllData["status"] = "success";
 $AllData['categories'] = $data ;
-getAllData('itmes','itme_discount	!= 0');
+$data = getAllData('items','itme_discount	!= 0',null,false);
 $AllData['items'] = $data ;
 echo json_encode($AllData);
